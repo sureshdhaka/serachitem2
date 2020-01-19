51b51fc47aa2e603a615320706794f69352235bb
sureshdhaka <35303093+sureshdhaka@users.noreply.github.com>
2020-01-19 22:23:29 +0530

    first commit

---
+++ src/java/movies3.java
@@ -0,0 +1,89 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+import pack.Movie;
+@WebServlet(urlPatterns = {"/movies3"})
+public class movies3 extends HttpServlet {
+@WebServiceRef(wsdlLocation="WEB-INF/wsdl/localhost_8080/github/movie.wsdl")
+    private movie_Service ser;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String p1=request.getParameter("jumanji");
+            String p2=request.getParameter("the dark knight");
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet movies3</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>movies are " + first(p1, p2) + "</h1>");
+               out.println("<h1>movies are " + first(p2,p1) + "</h1>");
+         
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+private String first(String p1, String p2) {
+       pack.Movie port=ser.getMoviesPort(); 
+return port.first(p1);
+    }
+
+    private static class movie_Service {
+
+        public movie_Service() {
+        }
+
+        private Movie getMoviesPort() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+}
